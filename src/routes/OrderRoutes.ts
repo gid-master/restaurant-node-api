dfc588660de39bc41a59b4eaf7fecf5cc0e000b5
rodrigotosta <rodrigotosta@gmail.com>
2021-03-15 15:40:33 -0300

    fixing guard API

--- src/routes/OrderRoutes.ts
+++ src/routes/OrderRoutes.ts
@@ -6,9 +6,9 @@ import { AuthMiddleware, MockMiddleware } from "@middlewares/index";
 class OrderRoutes extends BaseRoutes {
   public constructor () {
     super();
-    this.routes.post("/order", MockMiddleware.intercept, AuthMiddleware.authenticate, OrderController.createOrder);
-    this.routes.get("/order", MockMiddleware.intercept, AuthMiddleware.authenticate, OrderController.getAllOrders);
-    this.routes.post("/order/review", MockMiddleware.intercept, AuthMiddleware.authenticate, OrderController.reviewOrder);
+    this.routes.post("/order", MockMiddleware.intercept, AuthMiddleware.authenticated, OrderController.createOrder);
+    this.routes.get("/order", MockMiddleware.intercept, AuthMiddleware.authenticated, OrderController.getAllOrders);
+    this.routes.post("/order/review", MockMiddleware.intercept, AuthMiddleware.authenticated, OrderController.reviewOrder);
   }
 }
 
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -7,7 +7,7 @@ class UserRoutes extends BaseRoutes {
   public constructor () {
     super();
     this.routes.get("/user/authenticate", MockMiddleware.intercept, AuthMiddleware.authenticate, UserController.getUser);
-    this.routes.post("/user/push", MockMiddleware.intercept, AuthMiddleware.authenticate, UserController.pushPermission);
+    this.routes.post("/user/push", MockMiddleware.intercept, AuthMiddleware.authenticated, UserController.pushPermission);
     this.routes.post("/user/login", MockMiddleware.intercept, UserController.login);
     this.routes.post("/user/register", MockMiddleware.intercept, UserController.register);
   }
